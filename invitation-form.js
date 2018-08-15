4a85c53ca0b06f3f710b633aeedd55726f79bad6
kellshen <kellyshen7@gmail.com>
2018-08-15 18:49:40 +0000

    automatically backed up by learn

--- invitation-form.js
+++ invitation-form.js
@@ -5,3 +5,16 @@ document.addEventListener("DOMContentLoaded",
         document.getElementById("firstName").innerHTML += e.key;   
           
         })})
+        
+        
+
+
+function lettersOnly() {
+            var charCode = event.keyCode;
+
+            if ((charCode > 64 && charCode < 91) || (charCode > 96 && charCode < 123) || charCode == 8)
+
+                return true;
+            else
+                return false;
+}
