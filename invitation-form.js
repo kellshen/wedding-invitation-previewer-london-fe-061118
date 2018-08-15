285ba46ab522e69125dd1a34c703bb16e16b091e
kellshen <kellyshen7@gmail.com>
2018-08-15 18:05:07 +0000

    automatically backed up by learn

--- invitation-form.js
+++ invitation-form.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", 
     function() {
       console.log(document.getElementById("formFirstName"))
-        document.getElementById("formFirstName").addEventListener('oninput', function() {
+        document.getElementById("formFirstName").addEventListener('keypress', function() {
    console.log("Here")
    document.getElementById("firstName").value = this.value;   
 })})
