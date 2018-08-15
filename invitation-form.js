ba36e4ee62bdf3b253b8dd4b0b7b3578328681f2
kellshen <kellyshen7@gmail.com>
2018-08-15 18:43:07 +0000

    automatically backed up by learn

--- invitation-form.js
+++ invitation-form.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", 
     function() {
       console.log(document.getElementById("formFirstName"))
-        document.getElementById("formFirstName").addEventListener('input', function(e) {
+        document.getElementById("formFirstName").addEventListener('keydown', function(e) {
         document.getElementById("firstName").innerHTML += e.key;   
     
           
