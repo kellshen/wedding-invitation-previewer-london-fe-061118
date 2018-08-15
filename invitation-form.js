925007bfc141488c24c4fb99ec75aab340fa3234
kellshen <kellyshen7@gmail.com>
2018-08-15 17:59:27 +0000

    automatically backed up by learn

--- invitation-form.js
+++ invitation-form.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", 
     function() {
-      console.log("LOADED")
+      console.log(document.getElementById("formFirstName"))
         document.getElementById("formFirstName").addEventListener('onkeyup', function() {
    console.log("Here")
    document.getElementById("firstName").value = this.value;   
